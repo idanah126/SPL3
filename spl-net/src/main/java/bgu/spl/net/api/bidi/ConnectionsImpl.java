16612d2dff0115d47446cc4a7fca2165026bbbea
idanah126 <idan.aharoni2@gmail.com>
2021-12-27 15:27:33 +0200

    added files

---
+++ spl-net/src/main/java/bgu/spl/net/api/bidi/ConnectionsImpl.java
@@ -0,0 +1,18 @@
+package bgu.spl.net.api.bidi;
+
+public class ConnectionsImpl<T> implements Connections<T> {
+    @Override
+    public boolean send(int connectionId, T msg) {
+        return false;
+    }
+
+    @Override
+    public void broadcast(T msg) {
+
+    }
+
+    @Override
+    public void disconnect(int connectionId) {
+
+    }
+}
