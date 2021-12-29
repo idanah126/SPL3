a04389a59ea68d31a1671f1c4c9e39d2ce1624e3
idanah126 <idan.aharoni2@gmail.com>
2021-12-29 20:14:59 +0200

    שגקגק

---
+++ Boost_Echo_Client/cmake-build-debug/CMakeFiles/Boost_Echo_Client.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "Boost_Echo_Client.exe"
+  "Boost_Echo_Client.exe.manifest"
+  "Boost_Echo_Client.lib"
+  "Boost_Echo_Client.pdb"
+  "CMakeFiles/Boost_Echo_Client.dir/src/connectionHandler.cpp.obj"
+  "CMakeFiles/Boost_Echo_Client.dir/src/echoClient.cpp.obj"
+  "CMakeFiles/Boost_Echo_Client.dir/src/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Boost_Echo_Client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
