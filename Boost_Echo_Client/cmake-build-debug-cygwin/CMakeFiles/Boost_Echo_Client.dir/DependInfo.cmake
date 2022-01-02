b0771a1175d80f1ed1d0be88bb7192f209350507
mor abo <abom@post.bgu.co.il>
2022-01-03 01:05:13 +0200

    lk

---
+++ Boost_Echo_Client/cmake-build-debug-cygwin/CMakeFiles/Boost_Echo_Client.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/cygdrive/c/Users/morab/OneDrive/Desktop/works/spl/ass 3/SPL3/Boost_Echo_Client/src/connectionHandler.cpp" "/cygdrive/c/Users/morab/OneDrive/Desktop/works/spl/ass 3/SPL3/Boost_Echo_Client/cmake-build-debug-cygwin/CMakeFiles/Boost_Echo_Client.dir/src/connectionHandler.cpp.o"
+  "/cygdrive/c/Users/morab/OneDrive/Desktop/works/spl/ass 3/SPL3/Boost_Echo_Client/src/echoClient.cpp" "/cygdrive/c/Users/morab/OneDrive/Desktop/works/spl/ass 3/SPL3/Boost_Echo_Client/cmake-build-debug-cygwin/CMakeFiles/Boost_Echo_Client.dir/src/echoClient.cpp.o"
+  "/cygdrive/c/Users/morab/OneDrive/Desktop/works/spl/ass 3/SPL3/Boost_Echo_Client/src/main.cpp" "/cygdrive/c/Users/morab/OneDrive/Desktop/works/spl/ass 3/SPL3/Boost_Echo_Client/cmake-build-debug-cygwin/CMakeFiles/Boost_Echo_Client.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
